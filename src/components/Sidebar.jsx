6d9457063ecef59409552f5dda8e5da46ba2d800
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-04 09:37:06 +0500

    created sidebar and modified layout for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import Link from "next/link";
+import { FaXTwitter } from "react-icons/fa6";
+import { HiHome } from "react-icons/hi2";
+
 
 export default function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
+    return (
+        <div className="flex flex-col gap-4 pr-3">
+            <Link href={'/'}>
+                <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+            </Link>
+            <Link href={'/'} className="flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit">
+                <HiHome className="w-7 h-7 " />
+                <span className="font-bold hidden xl:inline">Home</span>
+            </Link>
+            <button className="bg-blue-500 hidden xl:inline text-white w-48 h-9 rounded-full px-4 py-2 hover:brightness-95 shadow-md transition-all duration-200">
+                Sign In
+            </button>
+        </div>
+    )
 }
